23b2f906f948b9fd763ff2a13b2f677eb6663eb0
anaraa@code.edu.az <you@example.com>
2023-06-21 05:03:39 -0700

    Whiskey

---
+++ EndProject/EndProject/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using EndProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EndProject.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
